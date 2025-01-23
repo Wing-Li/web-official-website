48e43d338252ff7462b453d7085888cff205cfc1
Wing_Li <iamlyl@foxmail.com>
2025-01-23 16:54:36 +0800

    feat(public): update metadata and add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Web Official Website",
-  description: "",
+  title: "NBBIT",
+  description: "The Cutting-Edge Binary Options Trading Platform",
 };
 
 export default function RootLayout({
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/logo.jpg" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
